3a5eec3509477dbbbcb53d4f54fc59671b130c60
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2023-06-03 01:36:20 +0300

    Some Fix and Added Forgot Password System and SignSystem Complete

---
+++ app/src/main/java/com/example/avanto/data/model/Video.java
@@ -0,0 +1,47 @@
+package com.example.avanto.data.model;
+
+public class Video {
+    private int img;
+    private String title;
+    private String filePath;
+    private int duration;
+
+    public Video(int img, String title, String filePath, int duration) {
+        this.img = img;
+        this.title = title;
+        this.filePath = filePath;
+        this.duration = duration;
+    }
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
--- app/src/main/java/com/example/avanto/ui/fragment/MusicFragment.java
+++ app/src/main/java/com/example/avanto/ui/fragment/MusicFragment.java
@@ -2,19 +2,28 @@ package com.example.avanto.ui.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.avanto.R;
+import com.example.avanto.data.model.Audio;
+import com.example.avanto.databinding.FragmentMusicBinding;
+
+import java.util.List;
 
 public class MusicFragment extends Fragment {
+
+    private FragmentMusicBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_music, container, false);
+        binding = FragmentMusicBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 }
