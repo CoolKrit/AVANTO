822a4a321cd9d0d0fe3f8628f45989a325092de4
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2023-06-07 22:55:55 +0300

    Some changes and Added Sort and Search Video

--- app/src/main/java/com/example/avanto/ui/activity/VideoFolderActivity.java
+++ app/src/main/java/com/example/avanto/ui/activity/VideoFolderActivity.java
@@ -1,33 +1,49 @@
 package com.example.avanto.ui.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.database.Cursor;
+import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.avanto.R;
+import com.example.avanto.data.model.Music;
 import com.example.avanto.data.model.Video;
 import com.example.avanto.databinding.ActivityHomeBinding;
 import com.example.avanto.databinding.ActivityVideoFolderBinding;
 import com.example.avanto.ui.stateholder.adapter.VideoAdapter;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
-public class VideoFolderActivity extends AppCompatActivity {
+public class VideoFolderActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
 
+    private static final String MY_SORT_PREF = "sortOrder";
     private ActivityVideoFolderBinding binding;
     private RecyclerView recyclerView;
     private String name;
     private ArrayList<Video> videoArrayList = new ArrayList<>();
     private VideoAdapter videoAdapter;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,19 +53,67 @@ public class VideoFolderActivity extends AppCompatActivity {
 
         name = getIntent().getStringExtra("folderName");
         recyclerView = binding.videoRecyclerview;
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        int index = name.lastIndexOf("/");
+        String onlyFolderName = name.substring(index + 1);
+        toolbar.setTitle(onlyFolderName);
         loadVideoList();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.video_search_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        ImageView ivClose = searchView.findViewById(androidx.appcompat.R.id.search_close_btn);
+        ivClose.setColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.white),
+                android.graphics.PorterDuff.Mode.SRC_IN);
+        searchView.setOnQueryTextListener(this);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     private void loadVideoList() {
-        videoArrayList = getAllVideoFromFolder(this, name);
+/*        videoArrayList = getAllVideoFromFolder(this, name);
         if (name != null && videoArrayList.size() > 0) {
             videoAdapter = new VideoAdapter(videoArrayList, this);
             recyclerView.setAdapter(videoAdapter);
             recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        }*/
+        videoArrayList = getAllVideoFromFolder(this, name);
+        if (name != null && videoArrayList.size() > 0) {
+            videoAdapter = new VideoAdapter(videoArrayList, this);
+            //if your recyclerview lagging then just add this line
+            recyclerView.setHasFixedSize(true);
+            recyclerView.setItemViewCacheSize(20);
+            recyclerView.setDrawingCacheEnabled(true);
+            recyclerView.setDrawingCacheQuality(View.DRAWING_CACHE_QUALITY_HIGH);
+            recyclerView.setNestedScrollingEnabled(false);
+
+            recyclerView.setAdapter(videoAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                    RecyclerView.VERTICAL, false));
+        } else {
+            Toast.makeText(this, "Can't find any videos", Toast.LENGTH_SHORT).show();
         }
     }
 
     private ArrayList<Video> getAllVideoFromFolder(Context context, String name) {
+        SharedPreferences preferences = getSharedPreferences(MY_SORT_PREF, MODE_PRIVATE);
+        String sort = preferences.getString("sorting", "sortByDate");
+        String order = null;
+        switch (sort) {
+            case "sortByDate":
+                order = MediaStore.MediaColumns.DATE_ADDED + " ASC";
+                break;
+            case "sortByName":
+                order = MediaStore.MediaColumns.DISPLAY_NAME + " ASC";
+                break;
+            case "sortBySize":
+                order = MediaStore.MediaColumns.SIZE + " DESC";
+                break;
+        }
+
         ArrayList<Video> VideoFiles = new ArrayList<>();
         Uri uri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
         String[] projection = {
@@ -63,10 +127,11 @@ public class VideoFolderActivity extends AppCompatActivity {
                 MediaStore.Video.Media.BUCKET_DISPLAY_NAME,
                 MediaStore.Video.Media.RESOLUTION
         };
-        String orderBy = MediaStore.Video.Media.DATE_ADDED + " DESC";
+
         String selection = MediaStore.Video.Media.DATA + " like?";
         String[] selectionArgs = new String[]{"%" + name + "%"};
-        Cursor cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, orderBy);
+
+        Cursor cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, order);
         if (cursor != null) {
             while (cursor.moveToNext()) {
                 String id = cursor.getString(0);
@@ -114,7 +179,55 @@ public class VideoFolderActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        SharedPreferences.Editor editor = getSharedPreferences(MY_SORT_PREF, MODE_PRIVATE).edit();
+        switch (item.getItemId()) {
+
+            case R.id.sort_by_date:
+                editor.putString("sorting", "sortByDate");
+                editor.apply();
+                this.recreate();
+                break;
+
+            case R.id.sort_by_name:
+                editor.putString("sorting", "sortByName");
+                editor.apply();
+                this.recreate();
+                break;
+
+            case R.id.sort_by_size:
+                editor.putString("sorting", "sortBySize");
+                editor.apply();
+                this.recreate();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        String input = newText.toLowerCase();
+        ArrayList<Video> searchList = new ArrayList<>();
+        for (Video model : videoArrayList) {
+            if (model.getTitle().toLowerCase().contains(input)) {
+                searchList.add(model);
+            }
+        }
+        videoAdapter.updateSearchList(searchList);
+
+        return false;
     }
 }
